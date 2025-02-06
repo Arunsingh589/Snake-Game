9b79b4877114949c490cce6ef937c2961dccf0fa
Arun Singh <126667359+Arunsingh589@users.noreply.github.com>
2025-02-06 17:18:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router'
 import LogIn from './Compoent/LogIn'
-import Signup from './Compoent/Signup'
+import Signup from './Compoent/Signup';
+import Snake from './Snakegame/Snake'
 const App = () => {
   return (
-    < >
-      <Router>
-        <div>
-          {/* <Link to={"/"}></Link> */}
-          <Signup />
-        </div>
-        <Routes>
-          <Route path='/login' element={<LogIn />} />
-          {/* <Route path='/' element={<Signup />} /> */}
-        </Routes>
-      </Router>
+    <>
+    <Snake />
+      // <Router>
+        // <div>
+        //   {/* <Link to={"/"}></Link> */}
+        //   <Signup />
+        // </div>
+        // <Routes>
+        //   <Route path='/login' element={<LogIn />} />
+        //   {/* <Route path='/' element={<Signup />} /> */}
+        // </Routes>
+      // </Router>
 
 
     </>
